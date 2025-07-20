1b03e51672ea36dad56783f376b3088e2e9d8951
Ganesh-Tata <ganeshtata115@gmail.com>
2025-07-20 07:57:18 +0530

    leetcode-1 Two Sum to leetcode-1 Two Sum.py

--- leetcode-1 Two Sum.py
+++ leetcode-1 Two Sum.py
@@ -1,4 +1,4 @@
-problem url: https://leetcode.com/problems/two-sum/
+# problem url: https://leetcode.com/problems/two-sum/
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
